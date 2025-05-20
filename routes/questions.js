b99f8bddefc638dad4fcf3e0bf224e6f64ee63b9
pfbouquet <pfbouquet@gmail.com>
2025-05-20 19:13:31 +0200

    fix game>games

--- routes/questions.js
+++ routes/questions.js
@@ -2,23 +2,22 @@ var express = require("express");
 var router = express.Router();
 
 const Questions = require("../database/models/Questions");
-const Game = require("../database/models/Game")
+const Game = require("../database/models/Games");
 
 /* GET questions of an idGame */
 router.get("/:gameID", function (req, res) {
-  Games.findOne({ _id: req.params.gameID }).populate("questions").then((data) => {
-
-    if (data) {
-      res.json({
-        result: true,
-        questions: data.questions,
-      })
-    } else {
-      res.json({ result: false, message: "impossible to access to gameID" })
-    }
-
-
-  })
+  Games.findOne({ _id: req.params.gameID })
+    .populate("questions")
+    .then((data) => {
+      if (data) {
+        res.json({
+          result: true,
+          questions: data.questions,
+        });
+      } else {
+        res.json({ result: false, message: "impossible to access to gameID" });
+      }
+    })
     .catch((error) => {
       console.log(error.message);
       res.json({
@@ -29,13 +28,16 @@ router.get("/:gameID", function (req, res) {
 });
 
 /* POST answers of a questionID */ // ====>>> à finir une fois la BDD test propre en place
-router.post('/answer', function (req, res) {
-  Questions.updateOne({ _id: req.body.questionID, playerID: req.body.playerID }, { answer: req.body.answer, answeredAtTime: req.body.answeredAtTime })
+router.post("/answer", function (req, res) {
+  Questions.updateOne(
+    { _id: req.body.questionID, playerID: req.body.playerID },
+    { answer: req.body.answer, answeredAtTime: req.body.answeredAtTime }
+  )
     .then((data) => {
       if (data) {
-        res.json({ result: true, question: data.answerHistory })
+        res.json({ result: true, question: data.answerHistory });
       } else {
-        res.json({result:false, message:"Answers NOT saved"})
+        res.json({ result: false, message: "Answers NOT saved" });
       }
     })
     .catch((error) => {
@@ -45,6 +47,6 @@ router.post('/answer', function (req, res) {
         message: error.message,
       });
     });
-})
+});
 
 module.exports = router;

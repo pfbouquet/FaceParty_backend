9648c61e1670b99b26fc98e06450deec9d14f47c
Aymeric <aymeric.simao@gmail.com>
2025-05-20 17:56:06 +0200

    Route GET Question done + route POST /questions/answer en cours

--- routes/questions.js
+++ routes/questions.js
@@ -2,15 +2,49 @@ var express = require("express");
 var router = express.Router();
 
 const Questions = require("../database/models/Questions");
+const Game = require("../database/models/Game")
 
 /* GET questions of an idGame */
-router.get("/:idGame", function (req, res) {
-  Questions.find().then((data) => {
-    res.json({
-      result: true,
-      data: data,
+router.get("/:gameID", function (req, res) {
+  Game.findOne({ _id: req.params.gameID }).populate("questions").then((data) => {
+
+    if (data) {
+      res.json({
+        result: true,
+        questions: data.questions,
+      })
+    } else {
+      res.json({ result: false, message: "impossible to access to gameID" })
+    }
+
+
+  })
+    .catch((error) => {
+      console.log(error.message);
+      res.json({
+        result: false,
+        message: error.message,
+      });
     });
-  });
 });
+
+/* POST answers of a questionID */ // ====>>> à finir une fois la BDD test propre en place
+router.post('/answer', function (req, res) {
+  Questions.updateOne({ _id: req.body.questionID, playerID: req.body.playerID }, { answer: req.body.answer, answeredAtTime: req.body.answeredAtTime })
+    .then((data) => {
+      if (data) {
+        res.json({ result: true, question: data.answerHistory })
+      } else {
+        res.json({result:false, message:"Answers NOT saved"})
+      }
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.json({
+        result: false,
+        message: error.message,
+      });
+    });
+})
 
 module.exports = router;

8932ba78ccdeff32280cb9ee4940e59d5708fa09
Aymeric <aymeric.simao@gmail.com>
2025-05-23 11:36:06 +0200

    ajout socket pour lancer la partie simultanément

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,10 +1,20 @@
 const sockets = async (io, socket) => {
+
+    // question en dur qui sera remplacé plus tard
     socket.on("question", (data) => {
         setTimeout(() => {
             io.emit("game-cycle", data)
         }, 500);
     })
 
+    // lancement de la partie par l'admin
+    socket.on("start-game", (roomID) => {
+        setTimeout(() => {
+            io.to(roomID).emit("lets-go")
+        }, 500);
+    })
+
+    // MàJ du nom du joueur
     socket.on("playerUpdate", (roomID) => {
         setTimeout(() => {
             io.to(roomID).emit("playerUpdate")
